dadf61ae0df11316181ea9c857aa079eb35f50a4
Matheus Viana <matheus.viana518062@gmail.com>
2022-07-03 22:52:23 -0300

    projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,13 @@
 document.addEventListener('scroll', () => {
     //topnav animation
     let topnav = document.getElementById('topnav')
+    let scrollTopBtn = document.getElementById('scrollTopBtn')
 
     if(window.scrollY > 15) {
         topnav.classList.add('topnav-anim')
+        scrollTopBtn.classList.add('show')
     }else{
         topnav.classList.remove('topnav-anim')
+        scrollTopBtn.classList.remove('show')
     }
 })
