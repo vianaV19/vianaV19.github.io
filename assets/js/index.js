15f9753668420dff770c4812f1f6b345278f09f2
Matheus Viana <matheus.viana518062@gmail.com>
2022-07-04 22:58:41 -0300

    many things

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,13 @@ document.addEventListener('scroll', () => {
         scrollTopBtn.classList.remove('show')
     }
 })
+
+
+function sendEmail() {
+    let nome = document.getElementById('nome')
+    let email = document.getElementById('email')
+    let bodyEmail = document.getElementById('body-email')
+
+    window.location.href = `mailto:matheus.viana518062@gmail.com?subject=${nome.value}&cc=${email.value}&body=${bodyEmail.value}`
+
+}
